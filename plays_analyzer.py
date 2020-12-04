48291aa2a723516de74cabfd131fae051c39eaf2
timwa <timwa@umich.edu>
2020-12-04 00:25:09 -0500

    started working on fetching album data using Spotipy API

--- plays_analyzer.py
+++ plays_analyzer.py
@@ -4,26 +4,47 @@ import os
 import requests
 import sqlite3
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+#from spotipy.oauth2 import SpotifyClientCredentials
 
 # Names
 # Timothy Wang - timwa@umich.edu
 # Emily Choe - emchoe@umich.edu
 
 #Spotipy API Info
-client_id = '9445bfc4e61f43ed83ec2782d464e42c'
-client_secret = 'f7fb2a1c147744d1ade617f598eabc46'
+CLIENT_ID = '9445bfc4e61f43ed83ec2782d464e42c'
+CLIENT_SECRET = 'f7fb2a1c147744d1ade617f598eabc46'
 
 #export SPOTIPY_CLIENT_ID='9445bfc4e61f43ed83ec2782d464e42c'
 #export SPOTIPY_CLIENT_SECRET='f7fb2a1c147744d1ade617f598eabc46'
 
-auth_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(auth_manager=auth_manager)
+#auth_manager = SpotifyClientCredentials()
+#sp = spotipy.Spotify(auth_manager=auth_manager)
 
-URI = 'spotify:playlist:37i9dQZF1DXcBWIGoYBM5M'
+#token = util.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+#cache_token = token.get_access_token()
+#sp = spotipy.Spotify(cache_token)
 
-album = sp.album_tracks(URI)
-print(album)
+#URI = 'spotify:playlist:37i9dQZF1DXcBWIGoYBM5M'
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+auth_response_data = auth_response.json()
+access_token = auth_response_data['access_token']
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+BASE_URL = 'https://api.spotify.com/v1/'
+playlist_id = '37i9dQZF1DXcBWIGoYBM5M'
+r = requests.get(BASE_URL + 'albums/' + playlist_id + '/tracks', headers=headers)
+print(r)
 
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
