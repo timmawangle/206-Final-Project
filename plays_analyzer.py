43c5e039f650346c7f70ccc11753586923fa886c
Emily Choe <emilychoe@Emilys-MacBook-Pro-4.local>
2020-12-08 21:10:19 -0500

    finished calculations and joined artists table to tracks_videos

--- plays_analyzer.py
+++ plays_analyzer.py
@@ -281,7 +281,8 @@ def get_video_list(video_tuples):
     return titles
 
 def join_tables(cur, conn):
-    cur.execute('SELECT Tracks.track_name, Tracks.artist_id, Tracks.pop_index, Videos.ranking FROM Tracks JOIN Videos ON Tracks.track_id=Videos.video_id ORDER BY pop_index DESC')
+    cur.execute('CREATE TABLE IF NOT EXISTS tracks_videos as SELECT Tracks.track_name, Tracks.artist_id, Tracks.pop_index, Videos.ranking FROM Tracks JOIN Videos ON Tracks.track_id=Videos.video_id ORDER BY pop_index DESC')
+    cur.execute('SELECT tracks_videos.track_name, Artists.name, tracks_videos.pop_index, tracks_videos.ranking FROM tracks_videos JOIN Artists ON tracks_videos.artist_id=Artists.artist_id')
     songs = cur.fetchall()
     #print(songs)
     difference_list = []
@@ -291,8 +292,8 @@ def join_tables(cur, conn):
     f = open(filename, "w")
 
     for i in range(len(songs)):
-        f.write("Spotify's ranking of " + str(songs[i][0]) + " is " + str(i + 1) + ". \n")
-        f.write("Youtube's ranking of " + str(songs[i][0]) + " is " + str(songs[i][3]) + ". \n")
+        f.write("Spotify's ranking of " + str(songs[i][0]) + " by " + str(songs[i][1]) + " is " + str(i + 1) + ". \n")
+        f.write("Youtube's ranking of " + str(songs[i][0]) + " by " + str(songs[i][1]) + " is " + str(songs[i][3]) + ". \n")
         diff = abs((i + 1) - (songs[i][3]))
         f.write("The difference in ranking is " + str(diff) + ". \n")
         f.write("\n")
